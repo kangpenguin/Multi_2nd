6e47e49f6b19a633e7d313e01cb3c1e9677bb5be
kangpenguin <rkddnjsrn25@naver.com>
2025-01-01 01:49:53 +0900

    20250101

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -1,5 +1,9 @@
+using UnityEngine;
+
 public class Dino : Entity
 {
+    public ParticleSystem dustParticle;
+
     protected override void Awake()
     {
         base.Awake();
@@ -33,5 +37,26 @@ public class Dino : Entity
     protected override void Die()
     {
         base.Die();
+    }
+
+    ///
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 착지 시 바닥 충돌 감지
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            PlayDustEffect();
+        }
+    }
+
+    void PlayDustEffect()
+    {
+        if (dustParticle != null)
+        {
+            // 먼지 파티클 재생
+            dustParticle.transform.position = transform.position + new Vector3(0, -0.3f, 0); // 플레이어 위치로 이동
+            dustParticle.Play();
+        }
     }
 }
